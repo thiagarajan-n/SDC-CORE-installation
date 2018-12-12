7c92806ef78c0bf7d0755603f57fd629c6f2511f
thiagu <thiagu.nagappan@rackspace.com>
2018-12-12 09:29:25 -0600

    add run_config script

---
+++ run_config.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+curl -o /tmp/sdc.tgz -L ${FULLTARBALL_URL}
+
+# for f in /tmp/*.tgz; do
+#         [ -e "$f" ] && mv "$f" /core-datacollctor/sdc.tgz || curl -o /tmp/sdc.tgz -L "${SDC_URL}"
+#         break
+# done
+
+tar xzf /tmp/sdc.tgz -C /opt/local/
+
+echo 'Done unzipping'
+ls /opt/local
+echo 'list unzipping'
+
+# cp /opt/local/streamsets-datacollector-3.6.1/systemd/sdc.service /etc/systemd/system/sdc.service
+# cp /opt/local/streamsets-datacollector-3.6.1/systemd/sdc.socket /etc/systemd/system/sdc.socket
+
+# cp -R /opt/local/streamsets-datacollector-3.6.1/etc/* /etc/sdc
+
+cp /opt/local/streamsets-datacollector-3.6.1/systemd/sdc.service /etc/systemd/system/sdc.service
+cp /opt/local/streamsets-datacollector-3.6.1/systemd/sdc.socket /etc/systemd/system/sdc.socket
+
+cp -R /opt/local/streamsets-datacollector-3.6.1/etc/* /etc/sdc
+ls /etc/sdc
+
